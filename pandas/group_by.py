9b9406747a7db4d9200fa06939cd0fe80418518c
Jonathan Bennett <jdbennet@ca.ibm.com>
2019-11-15 16:21:56 -0500

    Data generation

--- pandas/group_by.py
+++ pandas/group_by.py
@@ -3,34 +3,82 @@ import itertools
 import numpy as np
 import pandas as pd
 
-'''
-Walk through Natasha's use of Pandas to group data by a single field
-'''
+from datetime   import datetime, timedelta
 
 '''
-Seperate data into two groups, 'A' and 'B'
+Implement sample data + 'group by' support to test breakout detection 
 '''
-def get_id(count):
-    if count % 3 == 0:
-        return  "A"
-    else:
-         return  "B"
 
-data_set: list = []
+'''
+Round a floating point number to 'n' significant figures
+'''
+def round_to_n(x, n):
+    fmt = '{:1.' + str(n) + 'e}'    # gives 1.n figures
+    p = fmt.format(x).split('e')    # get mantissa and exponent
+                                    # round "extra" figure off mantissa
+    p[0] = str(round(float(p[0]) * 10**(n-1)) / 10**(n-1))
+    return float(p[0] + 'e' + p[1]) # convert str to float
 
-# Generate a hundred tuples of the form {value: number, id: string}
-for num in range(100):
-    value:dict = {"value": num, "id": "id_" + get_id(num)}
-    data_set.append(value).av
+'''
+ Generate a series of data points, one per minute
+ 
+ @typdef   {object} seed
+ @property {string} service
+ @property {number} requests (count number of incoming (server) datapoints)
+ @property {number} reliability ( (requests-errors) / requests)
+ @property {number} duration (cpu/requests)
+ 
+ @param     {seed}   seed        - basic service level
+ @param     {number} [start]     - integer minutes from start of data generation
+ @param     {number} [end]       - integer minutes from start of data generation
+ @param     {number} [variance]  - perecent deviation of results 
+ @return    {[seed]} 
+'''
+def data(seed, start, end, variance):
 
-df = pd.DataFrame(data_set)
-print(df)
+    # Let's pick an arbitrary starting point
+    start_time:datetime = datetime(year=2017, month=11, day=28, hour=23, minute=55)
 
-# group the data by id
-group = df.groupby("id")
+    request_level = seed['requests']
+    request_scale = request_level * variance
 
-group
+    reliability_level = seed['reliability']
+    error_level = int(request_level * ( 1 - reliability_level))
+    error_scale = error_level * variance
 
-print(group)
+    duration_level = seed['duration']
+    duration_scale = duration_level * variance
 
-# print(*data_set)
+    datapoints:list = []
+
+    for delta in range(start, end):
+
+        # timestamp
+        timestamp = start_time + timedelta(0, 0, 0, 0, delta)
+        timestamp = timestamp.strftime("%Y %m %d : %H:%M:%S")
+
+        # request data
+        requests = int(np.random.normal(loc=request_level, scale=request_scale))
+        errors   = int(np.random.normal(loc=error_level, scale=error_scale))
+
+        # statistics
+        reliability = (requests - errors) / requests
+        reliability = round_to_n(reliability, 3)
+
+        # duration
+        duration = int(np.random.normal(loc=duration_level, scale=duration_scale))
+
+        # result
+        datapoint = {"requests" : requests, "errors" : errors,
+                     "reliability" : reliability, "duration" : duration,
+                     "timestamp" : timestamp, "service" : seed['service']}
+
+        datapoints.append(datapoint)
+
+    return datapoints
+
+seed:dict = {"service": "data", "requests" : 500, "reliability": 0.99, "duration": 650}
+
+datapoints = data(seed, start=0, end=360, variance=0.1)
+print(datapoints)
+
